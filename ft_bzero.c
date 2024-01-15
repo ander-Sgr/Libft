6d8502b9350aa91942da6b9064e03cb2f949f8c0
Anderson <andersonbarreto2001@gmail.com>
2024-01-15 17:16:38 +0100

    adding functions bzero, memcpy,

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,27 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/11 17:22:19 by aestrell          #+#    #+#             */
-/*   Updated: 2024/01/11 17:22:19 by aestrell         ###   ########.fr       */
+/*   Created: 2024/01/13 13:10:39 by aestrell          #+#    #+#             */
+/*   Updated: 2024/01/13 13:10:39 by aestrell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <string.h>
+#include "libft.h"
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+/**
+ * rellna ceros en el puntero desde n 
+ * se ha de usar '\0'
+*/
+void	ft_bzero(void *s, size_t n)
+{
+	char	*temp_ptr;
+	size_t	i;
 
-#endif
+	i = 0;
+	temp_ptr = s;
+	while (i < n)
+	{
+		temp_ptr[i] = '\0';
+		i++;
+	}
+}
