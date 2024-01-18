01c5e4ab8a1c6273e1d46cf497ffecb23ea41a0d
Anderson <andersonbarreto2001@gmail.com>
2024-01-18 22:35:47 +0100

    adding strlcat strlcpy

---
+++ ft_strlcat.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/18 20:27:43 by aestrell          #+#    #+#             */
+/*   Updated: 2024/01/18 20:27:43 by aestrell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+
+size_t  ft_strlcat(char *dst, const char *src, size_t size)
+{
+    size_t  size_dst;
+    size_t  size_src;
+    size_t  total_size;
+    size_t  i;
+    
+    i = 0;
+    size_dst = ft_strlen(dst);
+    size_src = ft_strlen(src);
+    total_size = size_dst + size_src;
+    if (total_size > size)
+    {
+        return (total_size);
+    }
+    while (i < size && src[i] != '\0')
+    {
+        dst[size_dst] = src[i];
+        i++;
+        size_dst++;
+    }
+    dst[size_dst] = '\0';
+    return (size_dst);
+}
+
+
+/**
+ * concatena hasta el max dst es decir
+ * dst[5] = "hh"
+ * src[7] = "hola mundo"
+ * resultado sera -> hhol\0
+*/
---
+++ ft_strlcpy.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/18 18:33:42 by aestrell          #+#    #+#             */
+/*   Updated: 2024/01/18 18:33:42 by aestrell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t  ft_strlcpy(char *dst, const char *src, size_t size)
+{
+    size_t  size_src;
+    size_t  i;
+
+    i = 0;
+    
+    while (i < size - 1 && src[i] != '\0')
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    size_src = ft_strlen(src);
+    return (size_src);
+}
+
+/*
+#include <stdio.h>
+int main() {
+    char destino[1];
+    char origen[] = "Hola, mundo!";
+    
+    size_t tamano_destino = sizeof(destino);
+    
+    size_t resultado = ft_strlcpy(destino, origen, tamano_destino);
+    
+    // Imprimir el resultado
+    printf("Cadena copiada: %s\n", destino);
+    printf("Longitud total después de la copia: %zu\n", resultado);
+
+    return 0;
+}
+*/
