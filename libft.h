f3c16a8b9b99df7f473fd91ba8301a3630fff6e4
Anderson <andersonbarreto2001@gmail.com>
2024-01-30 19:47:45 +0100

    adding prototype

--- libft.h
+++ libft.h
@@ -9,9 +9,10 @@
 /*   Updated: 2024/01/11 17:22:19 by aestrell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <string.h>
+
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <string.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -23,7 +24,11 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-size_t  ft_strlcpy(char *dst, const char *src, size_t size);
-size_t  ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char    *ft_strrchr(const char *s, int c);
 
 #endif
