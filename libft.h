69bbe62d5d062b97e99775371295f6024b70766f
Anderson <andersonbarreto2001@gmail.com>
2024-02-05 21:39:22 +0100

    2da parte - substr.c

--- libft.h
+++ libft.h
@@ -13,6 +13,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <string.h>
+#include <stdlib.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -37,5 +38,6 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *nptr);
 void    *ft_calloc(size_t count, size_t size);
 char    *ft_strdup(const char *s1);
+char    *ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
