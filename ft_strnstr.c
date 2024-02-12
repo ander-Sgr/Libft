9fdba8228e71a2141407a32b6f76bfa560a29756
Anderson <andersonbarreto2001@gmail.com>
2024-02-13 00:43:39 +0100

    make fixes

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -18,14 +18,12 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	j;
 
 	i = 0;
-	if (haystack == NULL && len == 0)
-		return (NULL);
 	if (needle[0] == '\0')
 		return ((char *)haystack);
-	while (i < len && haystack[i] != '\0')
+	while (i < len && haystack[i])
 	{
 		j = 0;
-		while (needle[j] != '\0' && haystack[i + j] == needle[j])
+		while (needle[j] && haystack[i + j] == needle[j] && (i + j) < len)
 			j++;
 		if (needle[j] == '\0')
 			return ((char *)&haystack[i]);
--- ft_substr.c
+++ ft_substr.c
@@ -20,7 +20,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (s == NULL)
 		return (NULL);
 	if (start > ft_strlen(s))
-		return (NULL);
+	{
+		substr = (char *)malloc(sizeof(char));
+		substr[0] = '\0';
+		return (substr);
+	}
 	if ((start + len) > ft_strlen(s))
 		len = ft_strlen(s) - start;
 	substr = (char *)malloc((len + 1) * sizeof(char));
